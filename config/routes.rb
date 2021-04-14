75b1e4abf2c252877fdfb03782d254257f1f7cff
joesuffredini <gsuffred@gmail.com>
2021-04-14 13:59:15 -0400

    Initial commit, adds user, session controller and routes

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  # namespace :api do
+  #   get "/photos" => "photos#index"
+  # end
+
+  # Create routes for user create and user session
+  namespace :api do
+    post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
+  end
+
+end
