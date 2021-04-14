dd79a817b66ab709ec4ecbf538ea3d5575f434e0
joesuffredini <gsuffred@gmail.com>
2021-04-14 17:51:50 -0400

    Adds School create methods and verified with Insomnia

--- app/controllers/api/schools_controller.rb
+++ app/controllers/api/schools_controller.rb
@@ -1,2 +1,29 @@
 class Api::SchoolsController < ApplicationController
+
+
+  def create
+    @school = School.new({
+      id: params[:id],
+      name: params[:name],
+      location: params[:location],
+      nickname: params[:nickname],
+      enrollment: params[:enrollment],
+      conference: params[:conference],
+      rink: params[:rink],
+      head_coach: params[:head_coach],
+      assistant_coach1: params[:assistant_coach1],
+      assistant_coach2: params[:assistant_coach2],
+      hockey_office: params[:hockey_office],
+      email: params[:email],
+      })
+    # Happy/sad path testing
+    if  @school.save
+      render "show_schools.json.jb"
+    else 
+      render json: { errors: @school.errors.full_messages }, status: 406
+    end
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,11 +23,20 @@ Rails.application.routes.draw do
 
   # CRUD routes for Schools
   namespace :api do
-    get "/user" => "users#index"
-    get "/user/:id" => "users#show"
-    post "/user" => "users#create"
-    patch "/user/:id" => "users#update"
-    delete "/user/:id" => "users#destroy"
+    # get "/school" => "schools#index"
+    # get "/school/:id" => "schools#show"
+    post "/school" => "schools#create"
+    # patch "/school/:id" => "schools#update"
+    # delete "/school/:id" => "schools#destroy"
   end
+
+# CRUD routes for Recruiting Information
+# namespace :api do
+#   # get "/school" => "schools#index"
+#   # get "/school/:id" => "schools#show"
+#   # post "/school" => "schools#create"
+#   # patch "/school/:id" => "schools#update"
+#   # delete "/school/:id" => "schools#destroy"
+# end
 
 end
