a1f7741c88274987aaaaeaf767ed6239888bf828
joesuffredini <gsuffred@gmail.com>
2021-05-12 21:44:06 -0400

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -84,61 +84,61 @@ Recruit.create({player:	"Michael Stenberg",	position:	"F",	height:	"5 -10",	weig
 
 
 # This is for D1 College teams seeding
-  @teams = HTTP.get("http://api.sportradar.us/ncaamh-t3/league/hierarchy.json?sports_api[:api_key]")
-  @teams_info = @teams.parse
-  n = 0
-  i = 0
-  @d1teams = []
-  while n < 7
-      while i < @teams_info["divisions"][1]["conferences"][n]["teams"].length
-        teams = @teams_info["divisions"][1]["conferences"][n]["teams"][i]    
-        i = i + 1
-        @d1teams << teams
-      end
-    n = n + 1
-    i = 0
-  end
+  # @teams = HTTP.get("http://api.sportradar.us/ncaamh-t3/league/hierarchy.json?sports_api[:api_key]")
+  # @teams_info = @teams.parse
+  # n = 0
+  # i = 0
+  # @d1teams = []
+  # while n < 7
+  #     while i < @teams_info["divisions"][1]["conferences"][n]["teams"].length
+  #       teams = @teams_info["divisions"][1]["conferences"][n]["teams"][i]    
+  #       i = i + 1
+  #       @d1teams << teams
+  #     end
+  #   n = n + 1
+  #   i = 0
+  # end
 
-  @d1teams.map do |d1team|
-    School.create!(
-      teamid:  d1team["id"],
-      name: d1team["market"],
-      nickname: d1team["name"],
-      rink: d1team["venue"]["name"],
-      enrollment: d1team["venue"]["capacity"],
-    )
-  end
+  # @d1teams.map do |d1team|
+  #   School.create!(
+  #     teamid:  d1team["id"],
+  #     name: d1team["market"],
+  #     nickname: d1team["name"],
+  #     rink: d1team["venue"]["name"],
+  #     enrollment: d1team["venue"]["capacity"],
+  #   )
+  # end
 
 
-  # This is for roster seeding
+  # # This is for roster seeding
 
-  n = 66
-  teams = School.find_by(id:n)
-  key = teams.teamid
-  params = { :id => key}
-  @rosters = HTTP.get("http://api.sportradar.us/ncaamh-t3/teams/#{params[:id]}/profile.json?sports_api[:api_key]")
-  @rosters_info = @rosters.parse
+  # n = 66
+  # teams = School.find_by(id:n)
+  # key = teams.teamid
+  # params = { :id => key}
+  # @rosters = HTTP.get("http://api.sportradar.us/ncaamh-t3/teams/#{params[:id]}/profile.json?sports_api[:api_key]")
+  # @rosters_info = @rosters.parse
   
-  i = 0
-  @players = []
-  while i < @rosters_info["players"].length
-    player_name = @rosters_info["players"][i]["full_name"]
-    player_position = @rosters_info["players"][i]["position"]
-    player_shoots = @rosters_info["players"][i]["handedness"]
-    player_class = @rosters_info["players"][i]["experience"]
-    i = i + 1
-    @players << [player_name, player_position, player_shoots, player_class]
-  end
+  # i = 0
+  # @players = []
+  # while i < @rosters_info["players"].length
+  #   player_name = @rosters_info["players"][i]["full_name"]
+  #   player_position = @rosters_info["players"][i]["position"]
+  #   player_shoots = @rosters_info["players"][i]["handedness"]
+  #   player_class = @rosters_info["players"][i]["experience"]
+  #   i = i + 1
+  #   @players << [player_name, player_position, player_shoots, player_class]
+  # end
 
-  @players.map do |player|
-     Roster.create!(
-      name:  player[0],
-      position: player[1],
-      shoots:  player[2],
-      experience:   player[3],
-      school_id:  n,
-    )
-   end
+  # @players.map do |player|
+  #    Roster.create!(
+  #     name:  player[0],
+  #     position: player[1],
+  #     shoots:  player[2],
+  #     experience:   player[3],
+  #     school_id:  n,
+  #   )
+  #  end
 
 
   
