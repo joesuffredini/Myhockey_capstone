c56d4e980df535b106f0643eb8c2e83f4b1a5875
joesuffredini <gsuffred@gmail.com>
2021-05-14 17:08:11 -0400

    Updated database for heroku deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -85,20 +85,20 @@ User.create({name: "Dante Suffredini", city:"Detroit", state:"MI", birthdate:"08
 
 
 # This is for D1 College teams seeding
-  # @teams = HTTP.get("http://api.sportradar.us/ncaamh-t3/league/hierarchy.json?sports_api[:api_key]")
-  # @teams_info = @teams.parse
-  # n = 0
-  # i = 0
-  # @d1teams = []
-  # while n < 7
-  #     while i < @teams_info["divisions"][1]["conferences"][n]["teams"].length
-  #       teams = @teams_info["divisions"][1]["conferences"][n]["teams"][i]    
-  #       i = i + 1
-  #       @d1teams << teams
-  #     end
-  #   n = n + 1
-  #   i = 0
-  # end
+  @teams = HTTP.get("http://api.sportradar.us/ncaamh-t3/league/hierarchy.json?sports_api[:api_key]")
+  @teams_info = @teams.parse
+  n = 0
+  i = 0
+  @d1teams = []
+  while n < 7
+      while i < @teams_info["divisions"][1]["conferences"][n]["teams"].length
+        teams = @teams_info["divisions"][1]["conferences"][n]["teams"][i]    
+        i = i + 1
+        @d1teams << teams
+      end
+    n = n + 1
+    i = 0
+  end
 
   # @d1teams.map do |d1team|
   #   School.create!(
